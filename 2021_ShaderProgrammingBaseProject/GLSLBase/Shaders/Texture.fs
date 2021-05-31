a2337dfb5f8d3b74d1611d5ce8722f6a8c29215f
Kim Young June <rladudwns4643@gmail.com>
2021-05-31 21:37:01 +0900

    210531 classupdate(11-2)

--- 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/Texture.fs
+++ 2021_ShaderProgrammingBaseProject/GLSLBase/Shaders/Texture.fs
@@ -7,8 +7,24 @@ in vec2 v_TexPos;
 
 const float PI = 3.141592;
 
+vec4 P1()
+{
+	vec2 newTex = v_TexPos;
+	newTex.y = abs(newTex.y - 0.5) * 2.0;
+	vec4 returnColor =  texture(u_TexSampler, newTex);
+	return returnColor;
+}
+
+vec4 P2(){
+	vec2 newTex = v_TexPos;
+	newTex.x = fract(newTex.x*3.0);
+
+	vec4 returnColor = texture(u_TexSampler, newTex);
+	return returnColor;
+}
+
 void main()
 {
-	FragColor = vec4(v_TexPos, 0, 1);//texture(u_TexSampler, v_TexPos);
+	FragColor = P2();
 	
 }
